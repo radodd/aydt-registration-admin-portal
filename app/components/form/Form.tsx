b819794911efd371a043d2578e4e3f4e10e5be04
radodd <ethanf.flores@gmail.com>
2025-12-08 22:03:07 -0500

    feat: GitHub actions workflow

--- app/components/form/Form.tsx
+++ app/components/form/Form.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { FieldValues, FormProps, FormProvider, useForm } from "react-hook-form";
+// import React from "react";
+// import { FieldValues, FormProps, FormProvider, useForm } from "react-hook-form";
 
-const FormContext = React.createContext<ReturnType<typeof useForm> | null>(
-  null
-);
+// const FormContext = React.createContext<ReturnType<typeof useForm> | null>(
+//   null
+// );
 
-function Form<T extends FieldValues>({
-  onSubmit,
-  children,
-  ...methods
-}: FormProps<T>) {
-  return (
-    <FormProvider {...methods}>
-      <FormContext.Provider value={methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-5">
-          {children}
-        </form>
-      </FormContext.Provider>
-    </FormProvider>
-  );
-}
-export default Form;
+// function Form<T extends FieldValues>({
+//   onSubmit,
+//   children,
+//   ...methods
+// }: FormProps<T>) {
+//   return (
+//     <FormProvider {...methods}>
+//       <FormContext.Provider value={methods}>
+//         <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-5">
+//           {children}
+//         </form>
+//       </FormContext.Provider>
+//     </FormProvider>
+//   );
+// }
+// export default Form;
